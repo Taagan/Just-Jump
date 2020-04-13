a3101b1c1eceaf61d887534d8c72ad67eb709374
jesper-and <jesper.andersson17@outlook.com>
2020-04-13 22:23:18 +0200

    Fortsättning GUI
    fått musiken att pausa, lade till knappar på GUI. resume och restart funkar. de andra gör det inte än. Lade till ett skript för levelmanagement. Gjorde två metoder i detta för att loada levels och restarta nuvarande level.

--- Assets/GUIscript.cs
+++ Assets/GUIscript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GUIscript : MonoBehaviour
 {
+  
    public GameObject guiParentObject;
-    
+   static AudioSource audioSource;
 
     bool menuIsOpen;
     //float savedSpeed = MovementPlayer.thisInstance.speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = Camera.main.GetComponent<AudioSource>();
         menuIsOpen = false;
         if (guiParentObject.activeInHierarchy)
         {
@@ -29,6 +30,7 @@ public class GUIscript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseOrUnpause();
+            
         }
 
 
@@ -36,12 +38,14 @@ public class GUIscript : MonoBehaviour
         {
             Time.timeScale = 0;
             guiParentObject.SetActive(true);
+            audioSource.Pause();
             // Camera.main.GetComponent<AudioSource>().Pause();
         }
         else
         {
             Time.timeScale = 1;
             guiParentObject.SetActive(false);
+            audioSource.UnPause();
             //Camera.main.GetComponent<AudioSource>().Play();
         }
     }
---
+++ Assets/LevelManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManagerScript : MonoBehaviour
+{
+  public static int currentLevelIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        currentLevelIndex = levelIndex;
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void ReloadCurrentLevel()
+    {
+        SceneManager.LoadScene(currentLevelIndex);
+    }
+}
