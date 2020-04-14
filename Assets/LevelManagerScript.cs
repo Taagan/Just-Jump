ba9bbe642ad709fb7bdb0b49cfa0cc5e00780bf7
jesper-and <jesper.andersson17@outlook.com>
2020-04-14 16:09:14 +0200

    Continued work on GUI
    More edits on buttons, started work on progress slidebar

--- Assets/LevelManagerScript.cs
+++ Assets/LevelManagerScript.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class LevelManagerScript : MonoBehaviour
 {
   public static int currentLevelIndex = 0;
+
+    [SerializeField]
+    private Text author, songTitle;
+    public GameObject angledProgressBar, blackBackgroundProgressBar;
+   public string songTitleString, authorString;
+   public float totalSecondsInSong;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetCurrentSong();
     }
 
     // Update is called once per frame
@@ -28,5 +35,11 @@ public class LevelManagerScript : MonoBehaviour
     public void ReloadCurrentLevel()
     {
         SceneManager.LoadScene(currentLevelIndex);
+    }
+
+    public void GetCurrentSong()
+    {
+        songTitle.text = Camera.main.GetComponent<AudioSource>().clip.name;
+        totalSecondsInSong = Camera.main.GetComponent<AudioSource>().clip.length;
     }
 }
