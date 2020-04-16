238b813f5d25834ad070158ba18afb9c15c84354
jesper-and <jesper.andersson17@outlook.com>
2020-04-16 17:19:36 +0200

    ProgressBar
    Progress bar has been implemented, uses an empty gameobject at the end of each level so to calculate the distance needed to travel. Just fill the "Testobject" in the levelmanager script with whatever gameobject is at the end of the level.

--- Assets/LevelManagerScript.cs
+++ Assets/LevelManagerScript.cs
@@ -10,19 +10,43 @@ public class LevelManagerScript : MonoBehaviour
   public static int currentLevelIndex = 0;
 
     [SerializeField]
-    private Text author, songTitle;
-    public GameObject angledProgressBar, blackBackgroundProgressBar;
-   public string songTitleString, authorString;
-   public float totalSecondsInSong;
+    private Text author, songTitle, percentageText;
+    public Image angledProgressBar, blackBackgroundProgressBar;
+    [SerializeField]
+
+    float progressBarStartPosX, progressBarFinalPosX;
+    string songTitleString, authorString;
+    [SerializeField]
+
+    float totalSecondsInSong, xSpeed, distanceToTravel;
+    Vector3 startPos, endPos;
+    float testSeconds = 30;
+    public GameObject testObject;
+
+    private float startTime;
     // Start is called before the first frame update
     void Start()
     {
         GetCurrentSong();
+
+        startPos = angledProgressBar.transform.position;
+        endPos = blackBackgroundProgressBar.transform.position;
+        startTime = Time.time;
+        distanceToTravel = Vector3.Distance(gameObject.transform.position, testObject.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distCovered = (Time.time - startTime) * 7;
+        float fractionOfJourney = distCovered / distanceToTravel;
+       float t = gameObject.transform.position.x / testObject.transform.position.x;
+        float percent = t * 100f;
+        percentageText.text = percent.ToString("0.") + "%";
+        if (!GUIscript.menuIsOpen)
+        {
+            angledProgressBar.transform.position = Vector3.Lerp(startPos, endPos, t);
+        }
         
     }
 
