2e1f1f5e8cb49efcee53a4aad459365d03a8e0a6
jesper-and <jesper.andersson17@outlook.com>
2020-04-16 21:43:44 +0200

    Basic LevelSelect scene added.
    Really basic level select was added. For now only loads level and doesnt show stats for the level.
    - Also added a new folder for scripts.

--- Assets/scripts/LevelManagerScript.cs
+++ Assets/scripts/LevelManagerScript.cs
@@ -21,31 +21,39 @@ public class LevelManagerScript : MonoBehaviour
     float totalSecondsInSong, xSpeed, distanceToTravel;
     Vector3 startPos, endPos;
     float testSeconds = 30;
-    public GameObject testObject;
+    public GameObject endOfLevelMarker;
 
+   public bool inLevelSelect;
     private float startTime;
     // Start is called before the first frame update
     void Start()
     {
-        GetCurrentSong();
+        if (!inLevelSelect)
+        {
+            GetCurrentSong();
 
-        startPos = angledProgressBar.transform.position;
-        endPos = blackBackgroundProgressBar.transform.position;
-        startTime = Time.time;
-        distanceToTravel = Vector3.Distance(gameObject.transform.position, testObject.transform.position);
+            startPos = angledProgressBar.transform.position;
+            endPos = blackBackgroundProgressBar.transform.position;
+            startTime = Time.time;
+            distanceToTravel = Vector3.Distance(gameObject.transform.position, endOfLevelMarker.transform.position);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distCovered = (Time.time - startTime) * 7;
-        float fractionOfJourney = distCovered / distanceToTravel;
-       float t = gameObject.transform.position.x / testObject.transform.position.x;
-        float percent = t * 100f;
-        percentageText.text = percent.ToString("0.") + "%";
-        if (!GUIscript.menuIsOpen)
+        if (!inLevelSelect)
         {
-            angledProgressBar.transform.position = Vector3.Lerp(startPos, endPos, t);
+            float distCovered = (Time.time - startTime) * 7;
+            float fractionOfJourney = distCovered / distanceToTravel;
+           float t = gameObject.transform.position.x / endOfLevelMarker.transform.position.x;
+            float percent = t * 100f;
+            percentageText.text = percent.ToString("0.") + "%";
+            if (!GUIscript.menuIsOpen)
+            {
+                angledProgressBar.transform.position = Vector3.Lerp(startPos, endPos, t);
+            }
         }
         
     }
