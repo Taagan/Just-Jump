5f22b7d46a3a6992a37a4891dc632c127f48f1ac
Taagan <taagan.wennerberg@gmail.com>
2020-04-24 11:55:53 +0200

    Particle effects(Begun)

--- Assets/scripts/ChangeCamera.cs
+++ Assets/scripts/ChangeCamera.cs
@@ -6,24 +6,30 @@ public class ChangeCamera : MonoBehaviour
 {
     public Camera camera;
     public int zoomTo;
-    public string README = "Default Zoom is 7";
+    //public int zoomFactor;
+    private float targetZoom, lerpSpeed;
+    public string README = "Add the value you wish to make to the camera(7 is default)";
     // Start is called before the first frame update
     void Start()
     {
-
+        targetZoom = camera.orthographicSize;
+        lerpSpeed = 4f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-                camera.orthographicSize = zoomTo;
+            camera.orthographicSize = zoomTo;
+            //targetZoom += zoomFactor;
+            //camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, targetZoom, Time.deltaTime * lerpSpeed);
+
         }
 
     }
